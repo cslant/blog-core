4765afb27dbe256d765d96254569f5011a1880ea
Tan Nguyen <tannp27@gmail.com>
2024-07-28 01:52:38 +0700

    fix: move base code into blog core

---
+++ src/Http/Controllers/Base/BaseCategoryController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace CSlant\Blog\Core\Http\Controllers\Base;
+
+use Botble\Blog\Http\Controllers\API\CategoryController as BotbleCategoryController;
+
+class BaseCategoryController extends BotbleCategoryController
+{
+}
---
+++ src/Http/Controllers/Base/BasePostController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace CSlant\Blog\Core\Http\Controllers\Base;
+
+use Botble\Blog\Http\Controllers\API\PostController as BotblePostController;
+
+class BasePostController extends BotblePostController
+{
+}
