6e119795d2d824247f2163dd19738b84c1ee75cd
Tan Nguyen <tannp27@gmail.com>
2024-07-29 09:40:11 +0700

    #19 feat: add scout & elasticsearch config

---
+++ config/elasticsearch.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'host' => env('ELASTICSEARCH_HOST'),
+    'user' => env('ELASTICSEARCH_USER'),
+    'password' => env('ELASTICSEARCH_PASSWORD'),
+    'cloud_id' => env('ELASTICSEARCH_CLOUD_ID'),
+    'api_key' => env('ELASTICSEARCH_API_KEY'),
+    'ssl_verification' => env('ELASTICSEARCH_SSL_VERIFICATION', true),
+    'queue' => [
+        'timeout' => env('SCOUT_QUEUE_TIMEOUT'),
+    ],
+    'indices' => [
+        'mappings' => [
+            'default' => [
+                'properties' => [
+                    'id' => [
+                        'type' => 'keyword',
+                    ],
+                ],
+            ],
+        ],
+        'settings' => [
+            'default' => [
+                'number_of_shards' => 1,
+                'number_of_replicas' => 0,
+            ],
+        ],
+    ],
+];
