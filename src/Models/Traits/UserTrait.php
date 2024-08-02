eb5a560263e496c4348e53256c5e9cdd1cad3174
Tan Nguyen <tannp27@gmail.com>
2024-08-02 23:43:08 +0700

    fix: clean code

--- src/Models/Traits/UserTrait.php
+++ src/Models/Traits/UserTrait.php
@@ -19,11 +19,23 @@ trait UserTrait
     use Searchable;
     use SearchableAs;
 
+    /**
+     * Elastisearch config
+     * Get the index name for the model.
+     *
+     * @return string
+     */
     public function searchableAs(): string
     {
         return $this->userSearchableAs();
     }
 
+    /**
+     * Elastisearch config
+     * Get the indexable data array for the model.
+     *
+     * @return array
+     */
     public function toSearchableArray(): array
     {
         Log::info("UserTrait::toSearchableArray(), Es User: " . $this->getKey());
@@ -35,12 +47,23 @@ trait UserTrait
             'email_lowercase' => strtolower($this->email),
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
-            'first_name' => $this->first_name,
-            'last_name' => $this->last_name,
+            'first_name' => $this->first_name ?? null,
+            'last_name' => $this->last_name ?? null,
             'username' => $this->username,
-            'permissions' => $this->permissions,
+            'permissions' => $this->permissions ?? null,
             'avatar' => $this->avatar?->name ?? null,
             'avatar_url' => $this->avatar?->url ?? null,
         ];
+    }
+
+    /**
+     * Elastisearch config
+     * Determine if the model should be searchable.
+     *
+     * @return bool
+     */
+    public function shouldBeSearchable(): bool
+    {
+        return !$this->trashed();
     }
 }
