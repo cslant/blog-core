9be2453dd853a483cd3dc92ef83db69e3c7ca9b9
Tan Nguyen <tannp@cslant.com>
2024-11-11 16:22:21 +0700

    #6 feat: create the config to override filesystem configs of Laravel

--- config/filesystems.php
+++ config/filesystems.php
@@ -32,16 +32,45 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app'),
+            'root' => env('FILESYSTEM_FILE_ROOT', storage_path('app')),
+            'url' => env('ASSETS_URL', env('APP_URL').'/storage'),
             'throw' => false,
+            'permissions' => [
+                'file' => [
+                    'public' => 0664,
+                    'private' => 0600,
+                ],
+                'dir' => [
+                    'public' => 0775,
+                    'private' => 0700,
+                ],
+            ],
         ],
 
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('ASSETS_URL', env('APP_URL').'/storage'),
             'visibility' => 'public',
             'throw' => false,
+        ],
+
+        'prod' => [
+            'driver' => 'local',
+            'root' => env('FILESYSTEM_FILE_ROOT', storage_path('app')),
+            'url' => env('ASSETS_URL', env('APP_URL').'/storage'),
+            'throw' => false,
+            'visibility' => 'public',
+            'permissions' => [
+                'file' => [
+                    'public' => 0664,
+                    'private' => 0600,
+                ],
+                'dir' => [
+                    'public' => 0775,
+                    'private' => 0700,
+                ],
+            ],
         ],
 
         's3' => [
@@ -70,7 +99,7 @@ return [
     */
 
     'links' => [
-        public_path('storage') => storage_path('app/public'),
+        //
     ],
 
 ];
