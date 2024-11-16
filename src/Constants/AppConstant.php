f400bcc2d6f32fa36e26c3318fd90a7a68c84905
Tan Nguyen <tannp27@gmail.com>
2024-11-16 17:30:03 +0700

    feat: create app constants

---
+++ src/Constants/AppConstant.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace CSlant\Blog\Core\Constants;
+
+class AppConstant
+{
+    public const APP_NAME = 'CSlant Blog';
+
+    public const APP_AUTHOR = 'CSlant';
+
+    // App Environment: local, staging, production
+    public const APP_ENV_PROD = 'production';
+}
