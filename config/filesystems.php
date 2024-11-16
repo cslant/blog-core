ea547bd1509ff2c537c990aa7e7441f938a0e586
Tan Nguyen <tannp27@gmail.com>
2024-11-16 17:28:37 +0700

    fix: ensure the config storage link for prod env

--- config/filesystems.php
+++ config/filesystems.php
@@ -99,7 +99,8 @@ return [
     */
 
     'links' => [
-        public_path('storage') => env('FILESYSTEM_FILE_ROOT', storage_path('app/public')),
+        public_path('storage') => env('APP_ENV') === 'production'
+            ? env('FILESYSTEM_FILE_ROOT', storage_path('app/public'))
+            : storage_path('app/public'),
     ],
-
 ];
