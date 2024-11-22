b020e9ac9f36be772fff202671be1c63242a91ca
Tan Nguyen <tannp27@gmail.com>
2024-11-23 00:57:03 +0700

    chore: add admin url

--- config/blog-core.php
+++ config/blog-core.php
@@ -4,4 +4,6 @@ return [
     'name' => 'BlogCore',
 
     'fe_url' => env('BLOG_FE_URL', 'https://cslant.com/blog'),
+
+    'admin_url' => env('BLOG_ADMIN_URL', 'https://cslant.com/admin/blog'),
 ];
