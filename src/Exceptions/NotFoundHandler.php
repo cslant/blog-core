ff431f0f85a7ca3101fff9b6183326b921fed848
Tan Nguyen <tannp27@gmail.com>
2024-11-24 00:05:49 +0700

    feat: create handler to redirect 404 page if not found

---
+++ src/Exceptions/NotFoundHandler.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace CSlant\Blog\Core\Exceptions;
+
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Response;
+use Throwable;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class NotFoundHandler extends ExceptionHandler
+{
+    public function render($request, Throwable $e): Response|JsonResponse|RedirectResponse|\Symfony\Component\HttpFoundation\Response
+    {
+        if ($e instanceof NotFoundHttpException) {
+            return redirect('https://cslant.com/not-found', 301);
+        }
+
+        return parent::render($request, $e);
+    }
+}
