71dcb581af9fe34c601a098e062a3ddee6c2fa92
Tan Nguyen <tannp27@gmail.com>
2024-11-23 16:06:55 +0700

    fix: change the path config for api url

--- config/blog-core.php
+++ config/blog-core.php
@@ -10,4 +10,8 @@ return [
     'admin_path' => env('BLOG_ADMIN_PATH', 'admin/blog'),
     'admin_url' => env('BLOG_ADMIN_URL', 'https://cslant.com'),
     'admin_url_with_path' => env('BLOG_ADMIN_URL', 'https://cslant.com') . '/' . env('BLOG_ADMIN_PATH', 'admin/blog'),
+
+    'api_path' => env('BLOG_API_PATH', 'api/blog'),
+    'api_url' => env('BLOG_API_URL', 'https://cslant.com'),
+    'api_url_with_path' => env('BLOG_API_URL', 'https://cslant.com') . '/' . env('BLOG_API_PATH', 'api/blog'),
 ];
