b67f09d7272a09b350fc42e1d6e1b227d27a18e1
Tan Nguyen <tannp27@gmail.com>
2024-12-08 00:23:02 +0700

    feat: add new base models

---
+++ src/Models/Base/BasePost.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace CSlant\Blog\Core\Models\Base;
+
+use Botble\Blog\Models\Post as BotblePost;
+
+class BasePost extends BotblePost
+{
+}
--- src/Models/Post.php
+++ src/Models/Post.php
@@ -2,29 +2,32 @@
 
 namespace CSlant\Blog\Core\Models;
 
-use CSlant\Blog\Core\Models\Base\BaseCategory;
+use CSlant\Blog\Core\Models\Base\BasePost;
 
 /**
- * Class Category
+ * Class Post
  * @package CSlant\Blog\Core\Models
  *
  * @property int $id
  * @property string $name
- * @property int $parent_id
  * @property string $description
+ * @property string $content
  * @property string $status
  * @property int $author_id
  * @property string $author_type
- * @property string $icon
- * @property int $order
  * @property int $is_featured
- * @property int $is_default
+ * @property int $view
+ * @property string $format_type
  * @property string $created_at
  * @property string $updated_at
  * @property string $slug
  * @property string $url
- * @mixin BaseCategory
+ * @property array $tags
+ * @property array $categories
+ * @property string $image
+ * @property string $author
+ * @mixin BasePost
  */
-class Category extends BaseCategory
+class Post extends BasePost
 {
 }
