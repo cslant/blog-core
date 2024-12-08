fd16bdf6f4f96f2e446435f7598b790afdd347a9
Tan Nguyen <tannp27@gmail.com>
2024-12-08 10:26:28 +0700

    feat: add some method in docblock for responses

--- src/Http/Responses/Base/BaseHttpResponse.php
+++ src/Http/Responses/Base/BaseHttpResponse.php
@@ -3,11 +3,17 @@
 namespace CSlant\Blog\Core\Http\Responses\Base;
 
 use Botble\Base\Http\Responses\BaseHttpResponse as BotbleBaseHttpResponse;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Resources\Json\JsonResource;
 
 /**
  * Class BaseHttpResponse
  *
  * @package CSlant\Blog\Core\Http\Responses\Base
+ *
+ * @method self setData(mixed $data)
+ * @method self|JsonResponse|JsonResource|RedirectResponse toApiResponse()
  */
 class BaseHttpResponse extends BotbleBaseHttpResponse
 {
