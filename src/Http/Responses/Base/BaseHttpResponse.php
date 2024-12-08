c24ac3762ce8162293065340635c687e2ae497de
Tan Nguyen <tannp27@gmail.com>
2024-12-08 09:18:46 +0700

    feat: add new base responses

---
+++ src/Http/Responses/Base/BaseHttpResponse.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace CSlant\Blog\Core\Http\Responses\Base;
+
+use Botble\Base\Http\Responses\BaseHttpResponse as BotbleBaseHttpResponse;
+
+/**
+ * Class BaseHttpResponse
+ *
+ * @package CSlant\Blog\Core\Http\Responses\Base
+ *
+ * @method BaseHttpResponse setData(mixed $data) Set data to response from BaseHttpResponse class in Botble
+ */
+class BaseHttpResponse extends BotbleBaseHttpResponse
+{
+}
