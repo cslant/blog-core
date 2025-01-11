1c5130398411cedd6c9d51e0d7e5eddecd1c6890
Tan Nguyen <tannp27@gmail.com>
2025-01-12 02:47:46 +0700

    fix: analyse

--- src/Supports/Base/FilterPost.php
+++ src/Supports/Base/FilterPost.php
@@ -4,6 +4,13 @@ namespace CSlant\Blog\Core\Supports\Base;
 
 use Botble\Blog\Supports\FilterPost as BaseFilterPost;
 
+/**
+ * Class FilterPost
+ *
+ * @package CSlant\Blog\Core\Supports\Base
+ *
+ * @method self setFilters(array $request)
+ */
 class FilterPost extends BaseFilterPost
 {
 }
