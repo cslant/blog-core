ac014d2de450dd4060e6854cb89dabbac48b771b
Tan Nguyen <tannp27@gmail.com>
2025-01-14 03:32:25 +0700

    fix: ensure app content to depend php versions

--- src/Constants/AppConstant.php
+++ src/Constants/AppConstant.php
@@ -4,12 +4,12 @@ namespace CSlant\Blog\Core\Constants;
 
 class AppConstant
 {
-    public const string APP_NAME = 'CSlant Blog';
+    public const APP_NAME = 'CSlant Blog';
 
-    public const string APP_AUTHOR = 'CSlant';
+    public const APP_AUTHOR = 'CSlant';
 
     // App Environment: local, staging, production
-    public const string APP_ENV_PROD = 'production';
+    public const APP_ENV_PROD = 'production';
 
-    public const string DEFAULT_LOCALE = 'en';
+    public const DEFAULT_LOCALE = 'en';
 }
