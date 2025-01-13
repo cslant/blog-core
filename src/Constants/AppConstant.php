12e4e46e7f53f32de11a9d87ee4291ca59e7c513
Tan Nguyen <tannp27@gmail.com>
2025-01-14 03:14:36 +0700

    chore: add default locate constant

--- src/Constants/AppConstant.php
+++ src/Constants/AppConstant.php
@@ -4,10 +4,12 @@ namespace CSlant\Blog\Core\Constants;
 
 class AppConstant
 {
-    public const APP_NAME = 'CSlant Blog';
+    public const string APP_NAME = 'CSlant Blog';
 
-    public const APP_AUTHOR = 'CSlant';
+    public const string APP_AUTHOR = 'CSlant';
 
     // App Environment: local, staging, production
-    public const APP_ENV_PROD = 'production';
+    public const string APP_ENV_PROD = 'production';
+
+    public const string DEFAULT_LOCALE = 'en';
 }
