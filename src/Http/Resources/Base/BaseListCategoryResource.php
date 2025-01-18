047a7d94cc1df3de461fc0edb598c443441fb890
Tan Nguyen <tannp27@gmail.com>
2025-01-18 19:48:17 +0700

    feat: create base category resource

---
+++ src/Http/Resources/Base/BaseListCategoryResource.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace CSlant\Blog\Core\Http\Resources\Base;
+
+use Botble\Blog\Http\Resources\ListCategoryResource as BotbleListCategoryResource;
+
+class BaseListCategoryResource extends BotbleListCategoryResource
+{
+}
