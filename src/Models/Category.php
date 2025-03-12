096ac3ef279c3c2b87c6171a767c41d6d2672f69
Tan Nguyen <tannp27@gmail.com>
2025-03-13 00:31:55 +0700

    fix: update method return types in Category model for improved type hinting

--- src/Models/Category.php
+++ src/Models/Category.php
@@ -31,17 +31,17 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
  * @property Category $parent
  * @property Category[] $children
  *
- * @method static Builder<Category> newModelQuery()
- * @method static Builder<Category> newQuery()
- * @method static Builder<Category> query()
- * @method static Builder<Category> first()
- * @method static Builder<Category> find($id)
- * @method static Builder<Category> with($relations)
- * @method static Builder<Category> whereId($value)
- * @method static Builder<Category> whereIn($column, $values)
- * @method static Builder<Category> where($column, $operator = null, $value = null, $boolean = 'and')
- * @method static Category findOrFail($id)
- * @method static Category create($data)
+ * @method static Builder|Category newModelQuery()
+ * @method static Builder|Category newQuery()
+ * @method static Builder|Category query()
+ * @method static Builder|Category first()
+ * @method static Builder|Category find($id)
+ * @method static Builder|Category with($relations)
+ * @method static Builder|Category whereId($value)
+ * @method static Builder|Category whereIn($column, $values)
+ * @method static Builder|Category where($column, $operator = null, $value = null, $boolean = 'and')
+ * @method static Builder|Category findOrFail($id)
+ * @method static Builder|Category create($data)
  *
  * @mixin BaseCategory
  */
