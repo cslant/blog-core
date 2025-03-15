ba6ee0736b2fdea4d5bf695bfef9f074478d434d
Tan Nguyen <tannp27@gmail.com>
2025-03-15 11:07:44 +0700

    fix: update Swagger API URL environment variables for consistency

--- config/openapi.php
+++ config/openapi.php
@@ -1,10 +1,10 @@
 <?php
 
 defined('L5_SWAGGER_API_DEV_URL')
-|| define('L5_SWAGGER_API_DEV_URL', env('L5_SWAGGER_API_LOCAL_URL', 'http://localhost/api'));
+|| define('L5_SWAGGER_API_DEV_URL', env('L5_SWAGGER_API_DEV_URL', 'http://localhost/api'));
 
 defined('L5_SWAGGER_API_STAGING_URL')
-|| define('L5_SWAGGER_API_STAGING_URL', env('L5_SWAGGER_API_PROD_URL', 'https://staging.cslant.com/api'));
+|| define('L5_SWAGGER_API_STAGING_URL', env('L5_SWAGGER_API_STAGING_URL', 'https://staging.cslant.com/api'));
 
 defined('L5_SWAGGER_API_PROD_URL')
 || define('L5_SWAGGER_API_PROD_URL', env('L5_SWAGGER_API_PROD_URL', 'https://cslant.com/api'));
