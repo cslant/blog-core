e24f8c240e4504a1e3d53a09a623801c5773b32d
Tan Nguyen <tannp27@gmail.com>
2025-03-15 11:05:46 +0700

    fix: update Swagger API URLs in environment files and define dynamic URL constants

--- config/openapi.php
+++ config/openapi.php
@@ -1,10 +1,13 @@
 <?php
 
-defined('YOUR_DYNAMIC_URL_CONSTANT')
-|| define('YOUR_DYNAMIC_URL_CONSTANT', env('L5_SWAGGER_API_LOCAL_URL', 'http://localhost/api'));
+defined('L5_SWAGGER_API_DEV_URL')
+|| define('L5_SWAGGER_API_DEV_URL', env('L5_SWAGGER_API_LOCAL_URL', 'http://localhost/api'));
 
-defined('YOUR_DYNAMIC_PROD_URL_CONSTANT')
-|| define('YOUR_DYNAMIC_PROD_URL_CONSTANT', env('L5_SWAGGER_API_PROD_URL', 'https://cslant.com/api'));
+defined('L5_SWAGGER_API_STAGING_URL')
+|| define('L5_SWAGGER_API_STAGING_URL', env('L5_SWAGGER_API_PROD_URL', 'https://staging.cslant.com/api'));
+
+defined('L5_SWAGGER_API_PROD_URL')
+|| define('L5_SWAGGER_API_PROD_URL', env('L5_SWAGGER_API_PROD_URL', 'https://cslant.com/api'));
 
 return [
     //
