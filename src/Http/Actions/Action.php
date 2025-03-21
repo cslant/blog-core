8ce47d78ebe1b2f5c2aafdcf55f0905e3b6f8578
Tan Nguyen <tannp27@gmail.com>
2025-03-21 07:39:18 +0700

    refactor: update package and group annotations in Action class

--- src/Http/Actions/Action.php
+++ src/Http/Actions/Action.php
@@ -7,8 +7,8 @@ use Botble\Base\Http\Controllers\Concerns\HasHttpResponse;
 /**
  * Base Action class providing common functionality for API actions
  *
- * @package CSlant\Blog\Api\Http\Actions\Concerns
- * @group Blog API
+ * @package CSlant\Blog\Api\Http\Actions
+ * @group Blog Core
  */
 abstract class Action
 {
