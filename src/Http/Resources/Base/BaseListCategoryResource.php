a1319e72a1833d67c6f49d9f9b34088655dca57f
Tan Nguyen <tannp27@gmail.com>
2025-04-12 18:45:14 +0700

    chore: remove redundant classes

--- src/Http/Resources/Base/BaseListCategoryResource.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace CSlant\Blog\Core\Http\Resources\Base;
-
-use Botble\Blog\Http\Resources\ListCategoryResource as BotbleListCategoryResource;
-
-class BaseListCategoryResource extends BotbleListCategoryResource
-{
-}
--- src/Http/Resources/Base/BaseListPostResource.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace CSlant\Blog\Core\Http\Resources\Base;
-
-use Botble\Blog\Http\Resources\ListPostResource as BotbleListPostResource;
-
-class BaseListPostResource extends BotbleListPostResource
-{
-}
