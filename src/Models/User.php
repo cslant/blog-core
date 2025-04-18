6b85abe63df3dbfe536ba4a3ac32b0f5962b6f20
TonyDuong0509 <128566649+TonyDuong0509@users.noreply.github.com>
2025-04-18 07:44:11 +0000

    Fix styling

--- src/Models/User.php
+++ src/Models/User.php
@@ -9,7 +9,6 @@ use CSlant\LaravelLike\Enums\InteractionTypeEnum;
 use CSlant\LaravelLike\UserHasInteraction;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
