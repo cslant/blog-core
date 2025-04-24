2226270996bf1d10764fd05966fbd1781cc8fdd8
Xuan Thinh <pxthinh.vn@gmail.com>
2025-04-25 01:11:56 +0700

    feat: add property for comment

--- src/Models/Comment.php
+++ src/Models/Comment.php
@@ -10,6 +10,21 @@ use CSlant\Blog\Core\Models\Base\BaseComment;
  *
  * @package CSlant\Blog\Core\Models
  *
+ * @property int $id
+ * @property int $reply_to
+ * @property string $author_type
+ * @property int $author_id
+ * @property string $reference_type
+ * @property int $reference_id
+ * @property string $reference_url
+ * @property string $name
+ * @property string $email
+ * @property string $website
+ * @property string $content
+ * @property string $status
+ * @property string $ip_address
+ * @property string $user_agent
+ *
  * @mixin BaseComment
  */
 #[AllowDynamicProperties]
