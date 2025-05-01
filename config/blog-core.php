ed59cd7582d8f4e3055a3ad21bb1addcef4c9f93
Tan Nguyen <tannp27@gmail.com>
2025-05-01 16:53:47 +0700

    refactor: simplify URL generation by removing the generateUrlWithPath function

--- config/blog-core.php
+++ config/blog-core.php
@@ -1,27 +1,19 @@
 <?php
 
-function generateUrlWithPath(string $urlKey, string $pathKey, string $defaultUrl, string $defaultPath): string
-{
-    $url = env($urlKey, $defaultUrl);
-    $path = env($pathKey, $defaultPath);
-
-    return $path ? "{$url}/{$path}" : $url;
-}
-
 return [
     'name' => 'BlogCore',
 
     'fe_path' => env('BLOG_FE_PATH', 'blog'),
     'fe_url' => env('BLOG_FE_URL', 'https://cslant.com'),
-    'fe_url_with_path' => generateUrlWithPath('BLOG_FE_URL', 'BLOG_FE_PATH', 'https://cslant.com', 'blog'),
+    'fe_url_with_path' => env('BLOG_FE_URL', 'https://cslant.com') . (env('BLOG_FE_PATH', 'blog') ? '/' . env('BLOG_FE_PATH', 'blog') : ''),
 
     'admin_path' => env('BLOG_ADMIN_PATH', 'admin/blog'),
     'admin_url' => env('BLOG_ADMIN_URL', 'https://cslant.com'),
-    'admin_url_with_path' => generateUrlWithPath('BLOG_ADMIN_URL', 'BLOG_ADMIN_PATH', 'https://cslant.com', 'admin/blog'),
+    'admin_url_with_path' => env('BLOG_ADMIN_URL', 'https://cslant.com') . (env('BLOG_ADMIN_PATH', 'admin/blog') ? '/' . env('BLOG_ADMIN_PATH', 'admin/blog') : ''),
 
     'api_path' => env('BLOG_API_PATH', 'api/blog'),
     'api_url' => env('BLOG_API_URL', 'https://cslant.com'),
-    'api_url_with_path' => generateUrlWithPath('BLOG_API_URL', 'BLOG_API_PATH', 'https://cslant.com', 'api/blog'),
+    'api_url_with_path' => env('BLOG_API_URL', 'https://cslant.com') . (env('BLOG_API_PATH', 'api/blog') ? '/' . env('BLOG_API_PATH', 'api/blog') : ''),
 
     'laravel_domain' => env('LARAVEL_BLOG_DOMAIN', 'no-accept-domain'),
 
