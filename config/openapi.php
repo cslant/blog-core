1d2a4255e94f6420728cf8ccd068db13fddd545f
Tan Nguyen <tannp27@gmail.com>
2025-05-13 09:23:08 +0700

    feat: update OpenAPI documentation with environment variable support

--- config/openapi.php
+++ config/openapi.php
@@ -9,6 +9,9 @@ defined('L5_SWAGGER_API_STAGING_URL')
 defined('L5_SWAGGER_API_PROD_URL')
 || define('L5_SWAGGER_API_PROD_URL', env('L5_SWAGGER_API_PROD_URL', 'https://cslant.com/api'));
 
+defined('L5_SWAGGER_INFO_TITLE') || define('L5_SWAGGER_INFO_TITLE', env('L5_SWAGGER_INFO_TITLE', 'The API Documentation'));
+defined('L5_SWAGGER_INFO_DESCRIPTION') || define('L5_SWAGGER_INFO_DESCRIPTION', env('L5_SWAGGER_INFO_DESCRIPTION', 'The API Documentation for CSlant Blog - Build by cslant.com'));
+
 return [
     //
 ];
