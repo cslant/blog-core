887b79f2eccac91a99ead1675d702195fbe05def
tanhongit <35853002+tanhongit@users.noreply.github.com>
2025-05-13 15:23:52 +0000

    Fix styling

--- src/Commands/CheckSqlModeCommand.php
+++ src/Commands/CheckSqlModeCommand.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\DB;
 class CheckSqlModeCommand extends Command
 {
     protected $signature = 'check:sqlmode';
+
     protected $description = 'Check the current SQL mode of the database';
 
     public function handle()
