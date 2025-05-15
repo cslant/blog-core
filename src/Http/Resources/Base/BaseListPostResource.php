051df21a437ca8bb800d5f89d59860ddfb40b47c
Tan Nguyen <tannp27@gmail.com>
2025-05-15 18:18:51 +0700

    Revert "chore: remove redundant classes"
    This reverts commit a1319e72a1833d67c6f49d9f9b34088655dca57f.

---
+++ src/Http/Resources/Base/BaseListPostResource.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace CSlant\Blog\Core\Http\Resources\Base;
+
+use Botble\Blog\Http\Resources\ListPostResource as BotbleListPostResource;
+
+class BaseListPostResource extends BotbleListPostResource
+{
+}
