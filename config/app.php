691e3a0a59cdf44212a4f8fd3876a2ecb7677d3d
Tan Nguyen <tannp27@gmail.com>
2025-05-22 00:09:19 +0700

    feat: add application configuration file with default timezone setting

---
+++ config/app.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Application Timezone
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the default timezone for your application, which
+    | will be used by the PHP date and date-time functions. The timezone
+    | is set to "UTC" by default as it is suitable for most use cases.
+    |
+    */
+
+    'timezone' => env('APP_TIMEZONE', 'UTC'),
+];
